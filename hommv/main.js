92984469cb7a45371d9d4f1583fbad20dfe5c2fc
matiasasensi <114271414+matiasasensi@users.noreply.github.com>
2023-04-04 15:51:25 -0300

    Add files via upload

---
+++ hommv/main.js
@@ -0,0 +1,22 @@
+//let cantidad_de_petes = 69;
+
+    //let petes_sumados = parseInt(prompt("cuantos petes sumaste hoy?"));
+    //let petes_totales = cantidad_de_petes + petes_sumados; 
+    //if (petes_sumados==7){
+   // alert("ah pero tantos petes vas a hacer? goloso");}
+    //else {alert(petes_totales)}
+    var img = document.createElement("img");
+    img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+    var src = document.getElementById("id-of-element");
+    
+    function clickHandler() {
+      document.getElementById("id-of-element").innerHTML = "new text";
+              
+
+              
+              src.appendChild(img); 
+            }
+    
+
+      //if(button == active)
+      //set hover
